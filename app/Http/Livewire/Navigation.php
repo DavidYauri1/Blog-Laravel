d6f8281cd79ec9b9020ddb12e2b169ed46aa1dc4
DavidYauri1 <david.yauri@tecsup.edu.pe>
2022-08-11 22:34:52 -0500

    Menu Funcional

--- app/Http/Livewire/Navigation.php
+++ app/Http/Livewire/Navigation.php
@@ -3,11 +3,13 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Category;
 
 class Navigation extends Component
 {
     public function render()
     {
-        return view('livewire.navigation');
+        $categories = Category::All();
+        return view('livewire.navigation',compact('categories'));
     }
 }
